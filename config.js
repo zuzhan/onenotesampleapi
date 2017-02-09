76bb2ce2425ead8c70d053900fab1c77e7b7459f
zuzhan <zuzhanzzm@gmail.com>
2017-02-08 20:27:15 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //Live Connect API information
+    clientId: 'c5c33430-b680-4de1-9cd4-aca1657cf33b',
+    clientSecret: 'kLn410y9UHTizhtyGSdm6r3',
+    redirectUrl: 'http://onenoteapisample.azurewebsites.net/'
+};
